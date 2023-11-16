905e487e98e5b234349cf8abc2954e71a07695cc
shin0112 <wnslcosltimo12@gmail.com>
2023-11-17 02:10:37 +0900

    chore: modify error code

--- src/main/java/mumage/mumagebackend/exception/ErrCode.java
+++ src/main/java/mumage/mumagebackend/exception/ErrCode.java
@@ -11,7 +11,9 @@ public enum ErrCode {
     INVALID_NAME_FORMAT(HttpStatus.BAD_REQUEST.value(), "이름 형식이 잘못되었습니다."),
     INVALID_NICKNAME_FORMAT(HttpStatus.BAD_REQUEST.value(), "닉네임 형식이 잘못되었습니다."),
     DUPLICATED_ID(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 아이디입니다."),
-    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 닉네임입니다.");
+    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 닉네임입니다."),
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND.value(), "유저 정보를 찾을 수 없습니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST.value(), "비밀번호가 일치하지 않습니다.");
 
     private final int status;
     private final String message;
